87ef1d80917331a5a67f356e41abad5713b066fd
Nimai Garg <ngarg617@student.fuhsd.org>
2024-06-15 16:15:04 -0700

    added UTC time to the page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -14,7 +14,7 @@ const Home = () => {
             <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Hello, how are you today?</h2>
             <div className="flex flex-col gap-2">
               <h1 className="text-4xl font-extrabold lg:text-7xl">
-                {time}
+                {time} UTC
               </h1>
               <p className="text-lg font-medium text-sky-1 lg:text-2xl ">{date}</p>
             </div>
